600849f47e302d06ae12a2f76c34ecce56e519a1
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2024-12-16 22:04:11 +0100

    chore: removed Contact component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
-import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import ScrollToTop from "./components/ScrollToTop";
 import { useRef } from "react";
@@ -18,7 +17,6 @@ const App = () => {
       <main role="main" id="main-content">
         <Hero />
         <Projects />
-        <Contact />
       </main>
       <Footer />
 
